ff3775f1145f1b513b8bb9fd1e1640fa52cba352
ramanata12 <ramadanawinata@gmail.com>
2022-10-30 17:08:33 +0700

    Membuat compute,controller,index dan view

--- src/main/java/pws/projectKasir/compute.java
+++ src/main/java/pws/projectKasir/compute.java
@@ -8,6 +8,70 @@ package pws.projectKasir;
  *
  * @author MSi-GAMING
  */
+
 public class compute {
+      public int getTotal(int harga, int jumlah){
+        int total = harga * jumlah;
+        
+        return total;
+    }
     
+//    Diskon
+    public int getDiskon(int harga, int jumlah){
+        int total = harga * jumlah;
+        int diskon;
+        
+        if(total <= 10000){
+            diskon = total - (total * 0);
+        }
+        else if((total > 10000) && (total <= 50000)){
+            diskon = total - (total * 5/100);
+        }
+        else if( (total > 50000) && (total <= 100000)){
+            diskon = total - (total * 10/100);
+        }
+        else{
+            diskon = total - (total * 15/100);
+        }
+        
+        return diskon;
+    }
+
+    public int ketDiskon(int harga, int jumlah){
+        int total = harga * jumlah;
+        int diskon;
+        
+        if(total <= 10000){
+            diskon = 0;
+        }
+        else if((total > 10000) && (total <= 50000)){
+            diskon = 5;
+        }
+        else if((total > 50000) && (total <= 100000)){
+            diskon = 10;
+        }
+        else{
+            diskon = 15;
+        }
+        
+        return diskon;
+    }
+    
+    public String getKembalian(int uangBayar, int getPotongan){
+        int kembalian = uangBayar- getPotongan;
+        
+        String hasil;
+        
+        if(uangBayar >= getPotongan){
+            hasil = "Rp " + kembalian;
+        }
+        else{
+            hasil = "Uang ga cukup";
+        }
+        
+        return hasil;
+        
+        
+    }
 }
+
--- src/main/java/pws/projectKasir/controller.java
+++ src/main/java/pws/projectKasir/controller.java
@@ -4,10 +4,50 @@
  */
 package pws.projectKasir;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pws.projectKasir.compute;
+
 /**
  *
  * @author MSi-GAMING
  */
+@Controller
 public class controller {
     
+    compute data = new compute();
+    
+    @RequestMapping("/hasil")
+    public String buyer(@RequestParam(value="nama") String nama,
+                        @RequestParam(value="harga") String harga,
+                        @RequestParam(value="jumlah") String jumlah,
+                        @RequestParam(value="bayar") String bayar,
+                        Model model){
+        
+        int hrg, jml, totBarang, diskon, laba, byr;
+        
+        hrg = Integer.parseInt(harga);
+        jml = Integer.parseInt(jumlah);
+        byr = Integer.parseInt(bayar);
+        
+        // Proses Perhitungan
+        totBarang = data.getTotal(hrg, jml);
+        diskon = data.getDiskon(hrg, jml);
+        laba = data.ketDiskon(hrg, jml);
+        String Kembalian = data.getKembalian(byr, diskon);
+        
+        model.addAttribute("nama", nama);
+        model.addAttribute("harga","Rp " +hrg);
+        model.addAttribute("jumlah", jml);
+        model.addAttribute("total", "Rp " + totBarang); // total sebelum diskon
+        model.addAttribute("diskon", "Rp " + diskon ); // total akhir
+        model.addAttribute("kembalian", Kembalian); // kembalian
+        model.addAttribute("bayar","Rp " +  byr); // Bayar
+        model.addAttribute("laba", laba + " %");
+        
+        
+        return "view";
+    }
 }
