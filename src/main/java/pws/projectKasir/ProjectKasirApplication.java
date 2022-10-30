bbb93a21b9facf32aaa934d5c1ed575e8f6ef2c5
ramanata12 <ramadanawinata@gmail.com>
2022-10-30 13:52:21 +0700

    Buat Projek

---
+++ src/main/java/pws/projectKasir/ProjectKasirApplication.java
@@ -0,0 +1,13 @@
+package pws.projectKasir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKasirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKasirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/projectKasir/ProjectKasirApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.projectKasir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKasirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
